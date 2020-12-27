18d57fa5a527c90a9353c84b348d1716077fc476
Gray7410 <gray7410@yandex.ru>
2020-12-27 22:47:02 +0500

    burger

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,11 @@ $(function(){
 
   
 });
+
+const menuToggle = document.querySelector("#menu-toggle");
+const mobileNav = document.querySelector("#mobile-nav");
+
+menuToggle.onclick = function() {
+	menuToggle.classList.toggle('header__burger--active');
+	mobileNav.classList.toggle('mobile-nav--active');
+}
